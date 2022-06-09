9c3a5d76190a61dd482ef0edc46f1f4bfc53a751
Piotr Kowalski <piecioshka@gmail.com>
2022-06-09 22:03:33 +0200

    Cleanup

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -25,11 +25,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      useHash: true,
-    }),
-  ],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class CoreRoutingModule {}
